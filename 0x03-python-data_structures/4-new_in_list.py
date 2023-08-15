0f9ecac2cfed3c75ed62d7256383ff0f108ea458
Gloriariechi99 <gloriariechi99@gmail.com>
2023-08-15 13:26:07 +0300

    task4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    
+    list1 = my_list.copy()
+
+    if idx >= 0 and idx <= (len(my_list) - 1):
+        list1[idx] = element
+    return list1
